59b6eb6efab8addc655480739f0f78d2a97415f5
koenderaden <koenderaden@gmail.com>
2025-01-05 14:54:25 +0100

    hero image added

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+
 export default function Hero() {
-  return <div>Hero Component</div>;
+  return (
+    <div className="relative w-full h-screen">
+      {/* Background image */}
+      <div className="absolute top-0 left-0 w-full h-full">
+        <Image
+          src="/imagekoen.png"
+          alt="Hero Background"
+          layout="fill"
+          quality={100}
+        />
+      </div>
+    </div>
+  );
 }
