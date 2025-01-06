146492a7fc3c93ed96388ff2f0346990ea77722c
koenderaden <koenderaden@gmail.com>
2025-01-06 14:06:15 +0100

    stars added back and hover effect added gallery

--- app/components/gallery.tsx
+++ app/components/gallery.tsx
@@ -59,70 +59,68 @@ export default function Gallery() {
   ];
 
   return (
-    <section className="py-20 bg-background">
+    <section className="py-20 bg-background relative">
       <div className="container mx-auto px-[60px]">
         {/* Gallery title */}
         <h2 className="font-spartan text-[4rem] font-semibold mb-12 text-text-primary">
           Gallery
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 relative">
           {projects.map((project, index) => (
             <div
               key={project.id}
               className="relative group w-[424px] h-[400px] rounded-lg border-[1px] border-white overflow-hidden cursor-pointer"
             >
               {/* Image */}
-              <Image
-                src={project.image.src}
-                alt={project.alt}
-                width={project.image.width}
-                height={project.image.height}
-                className="object-cover transition-transform duration-300 group-hover:scale-110"
-              />
+              <div className="w-full h-full overflow-hidden rounded-[15px]">
+                <Image
+                  src={project.image.src}
+                  alt={project.alt}
+                  width={project.image.width}
+                  height={project.image.height}
+                  className="object-cover transition-transform duration-300 group-hover:scale-110"
+                />
+              </div>
 
               {/* Hover Overlay */}
-              <div className="absolute inset-0 bg-black bg-opacity-85 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+              <div className="absolute inset-0 bg-black bg-opacity-85 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center rounded-[15px]">
                 <p className="font-spartan text-xl md:text-2xl font-semibold underline text-white">
                   {project.alt}
                 </p>
               </div>
-
-              {/* Starsright on Project 3 */}
-              {index === 2 && (
-                <div
-                  className="absolute"
-                  style={{
-                    top: "-50px",
-                    right: "-50px",
-                  }}
-                >
-                  <img
-                    src="/starsright.svg"
-                    alt="Stars Right"
-                    className="w-[64px] h-[64px]"
-                  />
-                </div>
-              )}
-
-              {/* Starsleft on Project 4 */}
-              {index === 3 && (
-                <div
-                  className="absolute"
-                  style={{
-                    bottom: "-50px",
-                    left: "-50px",
-                  }}
-                >
-                  <img
-                    src="/starsleft.svg"
-                    alt="Stars Left"
-                    className="w-[64px] h-[64px]"
-                  />
-                </div>
-              )}
             </div>
           ))}
+
+          {/* Starsright */}
+          <div
+            className="absolute z-50 pointer-events-none"
+            style={{
+              top: "calc(-133px + 100px)", // Positioneer boven project 3
+              right: "-10px",
+            }}
+          >
+            <img
+              src="/starsright.svg"
+              alt="Stars Right"
+              className="w-[64px] h-[64px]"
+            />
+          </div>
+
+          {/* Starsleft */}
+          <div
+            className="absolute z-50 pointer-events-none"
+            style={{
+              top: "calc(600px + 200px)", // Positioneer boven project 4
+              left: "-33px",
+            }}
+          >
+            <img
+              src="/starsleft.svg"
+              alt="Stars Left"
+              className="w-[64px] h-[64px]"
+            />
+          </div>
         </div>
       </div>
     </section>
