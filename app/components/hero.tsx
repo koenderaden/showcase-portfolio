88024c28ec9a66c471238313f93d69ab196d6629
koenderaden <koenderaden@gmail.com>
2025-01-06 14:58:54 +0100

    animation added hero lines

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,6 +1,20 @@
+"use client"; 
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Hero() {
+  const [showFirstText, setShowFirstText] = useState(false);
+  const [showSecondText, setShowSecondText] = useState(false);
+  const [showThirdText, setShowThirdText] = useState(false);
+
+  // Stel de vertraging in om de tekst een voor een te laten verschijnen
+  useEffect(() => {
+    setTimeout(() => setShowFirstText(true), 250); // Eerste tekst  na 0,25 seconden
+    setTimeout(() => setShowSecondText(true), 750); // Tweede tekst na 0,75 seconden
+    setTimeout(() => setShowThirdText(true), 1250); // Derde tekst na 1,25 seconde
+  }, []);
+
   return (
     <section className="h-screen relative overflow-hidden">
       {/* Achtergrondafbeelding */}
@@ -16,18 +30,30 @@ export default function Hero() {
       {/* Content */}
       <div className="relative z-10 h-full flex flex-col justify-center">
         <div className="container mx-auto px-[60px]">
-          {/* Bovenste tekst  */}
-          <p className="font-spartan text-3xl md:text-4xl font-semibold mb-0 text-left">
+          {/* Eerste zin */}
+          <p
+            className={`font-spartan text-3xl md:text-4xl font-semibold mb-0 text-left transition-opacity duration-1000 ${
+              showFirstText ? "opacity-100" : "opacity-0"
+            }`}
+          >
             Hi, my name is
           </p>
 
-          {/* Naam */}
-          <h1 className="font-spartan text-6xl md:text-9xl font-bold mb-0 text-left">
+          {/* Tweede zin */}
+          <h1
+            className={`font-spartan text-6xl md:text-9xl font-bold mb-0 text-left transition-opacity duration-1000 ${
+              showSecondText ? "opacity-100" : "opacity-0"
+            }`}
+          >
             Koen de Raden
           </h1>
 
-          {/* Onderste tekst*/}
-          <p className="font-spartan text-3xl md:text-4xl font-semibold mt-1 text-left">
+          {/* Derde zin */}
+          <p
+            className={`font-spartan text-3xl md:text-4xl font-semibold mt-1 text-left transition-opacity duration-1000 ${
+              showThirdText ? "opacity-100" : "opacity-0"
+            }`}
+          >
             ICT & Media Design Student
           </p>
         </div>
