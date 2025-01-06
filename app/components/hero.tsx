4da3b6a267f124e894e8b97b4855032df51ac8e2
koenderaden <koenderaden@gmail.com>
2025-01-06 13:10:44 +0100

    fonts added an lines added hero

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,16 +3,33 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <section className="h-screen relative overflow-hidden">
+      {/* Achtergrondafbeelding */}
       <Image
         src="/imagekoen.png"
         alt="Hero background"
         fill
         className="object-cover"
       />
-      <div className="absolute inset-0 bg-black bg-opacity-50" />
+      {/* Donkere overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-25" />
+
+      {/* Content */}
       <div className="relative z-10 h-full flex flex-col justify-center">
         <div className="container mx-auto px-[60px]">
-          <h1 className="text-6xl md:text-8xl font-bold mb-6">Koen de Raden</h1>
+          {/* Bovenste tekst  */}
+          <p className="font-spartan text-4xl md:text-5xl font-semibold mb-0 text-left">
+            Hi, my name is
+          </p>
+
+          {/* Naam */}
+          <h1 className="font-spartan text-7xl md:text-9xl font-bold mb-0 text-left">
+            Koen de Raden
+          </h1>
+
+          {/* Onderste tekst*/}
+          <p className="font-spartan text-4xl md:text-5xl font-semibold mt-1 text-left">
+            ICT & Media Design Student
+          </p>
         </div>
       </div>
     </section>
