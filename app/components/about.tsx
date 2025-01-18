5a4ff0ee6d15ed391e4bc0d5c9f52d172fc80349
koenderaden <koenderaden@gmail.com>
2025-01-18 14:31:10 +0100

    made apart sections for my skills + Added next.js logo's

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function About() {
-  const logos = [
+  const codingSkills = [
     {
       id: 1,
       name: "HTML",
@@ -16,34 +19,44 @@ export default function About() {
     },
     {
       id: 3,
-      name: "Figma",
-      bw: "/svgs/figma-bw.svg",
-      color: "/svgs/figma-color.svg",
-      link: "https://www.figma.com",
-    },
-    {
-      id: 4,
-      name: "JS",
+      name: "JavaScript",
       bw: "/svgs/js-bw.svg",
       color: "/svgs/js-color.svg",
       link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     },
     {
-      id: 5,
+      id: 4,
       name: "React",
       bw: "/svgs/react-bw.svg",
       color: "/svgs/react-color.svg",
       link: "https://reactjs.org",
     },
     {
+      id: 5,
+      name: "Next.js",
+      bw: "/svgs/next-bw.svg",
+      color: "/svgs/next-color.svg",
+      link: "https://nextjs.org/",
+    },
+    {
       id: 6,
-      name: "GitHub",
-      bw: "/svgs/github-bw.svg",
-      color: "/svgs/github-color.svg",
-      link: "https://github.com",
+      name: "Tailwind CSS",
+      bw: "/svgs/tailwind-bw.svg",
+      color: "/svgs/tailwind-color.svg",
+      link: "https://tailwindcss.com/",
     },
     {
       id: 7,
+      name: "Ionic Framework",
+      bw: "/svgs/ionic-bw.svg",
+      color: "/svgs/ionic-color.svg",
+      link: "https://ionicframework.com/",
+    },
+  ];
+
+  const cmsSkills = [
+    {
+      id: 8,
       name: "Storyblok",
       bw: "/svgs/storyblok-bw.svg",
       color: "/svgs/storyblok-color.svg",
@@ -51,54 +64,67 @@ export default function About() {
     },
   ];
 
+  const designSkills = [
+    {
+      id: 9,
+      name: "Figma",
+      bw: "/svgs/figma-bw.svg",
+      color: "/svgs/figma-color.svg",
+      link: "https://www.figma.com",
+    },
+  ];
+
+  const versionControlSkills = [
+    {
+      id: 10,
+      name: "GitHub",
+      bw: "/svgs/github-bw.svg",
+      color: "/svgs/github-color.svg",
+      link: "https://github.com",
+    },
+  ];
+
   return (
     <section className="py-20 bg-background">
       <div className="container mx-auto px-[60px]">
-        <div className="flex flex-col md:flex-col md:items-start">
-          {/* Title */}
-          <h2
-            className="text-[36px] font-semibold mb-8 md:mb-0 md:w-1/3 font-spartan"
-            style={{ marginBottom: "2rem" }}
-          >
-            About me
-          </h2>
+        <h2
+          className="text-[36px] font-semibold mb-8 md:mb-0 md:w-1/3 font-spartan"
+          style={{ marginBottom: "2rem" }}
+        >
+          About me
+        </h2>
+        <div
+          className="md:w-full text-[1rem] font-roboto"
+          style={{ marginBottom: "6rem" }}
+        >
+          <p className="mb-6">
+            Hi, my name is Koen de Raden, and I'm an 18-year-old from Waalwijk,
+            the Netherlands.
+          </p>
+          <p className="mb-6">
+            During my time at Dr. Mollercollege, I discovered my passion for
+            ICT. Choosing Informatics as an elective in my third year turned out
+            to be the perfect decision, sparking my interest in the world of
+            technology and digital innovation.
+          </p>
+          <p className="mb-6">
+            Outside of school, I love visiting theme parks, gaming, and watching
+            movies and series. For the past year and a half, I've also worked at
+            the Efteling, primarily at the Pirańa attraction. This job has been
+            a fantastic experience, teaching me about teamwork, responsibility,
+            and customer interaction—all while having fun!
+          </p>
+          <p className="mb-1">
+            My passion for ICT, combined with my creative hobbies and work
+            experience, drives me to explore and grow in this exciting field.
+          </p>
+        </div>
 
-          {/* Text Section */}
-          <div
-            className="md:w-full text-[1rem] font-roboto"
-            style={{ marginBottom: "6rem" }}
-          >
-            <p className="mb-6">
-              Hi, my name is Koen de Raden, and I'm an 18-year-old from
-              Waalwijk, the Netherlands.
-            </p>
-            <p className="mb-6">
-              During my time at Dr. Mollercollege, I discovered my passion for
-              ICT. Choosing Informatics as an elective in my third year turned
-              out to be the perfect decision, sparking my interest in the world
-              of technology and digital innovation.
-            </p>
-            <p className="mb-6">
-              Outside of school, I love visiting theme parks, gaming, and
-              watching movies and series. For the past year and a half, I've
-              also worked at the Efteling, primarily at the Pirańa attraction.
-              This job has been a fantastic experience, teaching me about
-              teamwork, responsibility, and customer interaction—all while
-              having fun!
-            </p>
-            <p className="mb-1">
-              My passion for ICT, combined with my creative hobbies and work
-              experience, drives me to explore and grow in this exciting field.
-            </p>
-          </div>
-
-          {/* Skills Section */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
           <div>
-            <h3 className="text-[36px] font-semibold mb-6 font-spartan">
-              My Skills
-            </h3>
+            <h4 className="text-2xl font-semibold mb-4">Coding Skills</h4>
             <div className="flex gap-4 flex-wrap">
-              {logos.map((logo) => (
+              {codingSkills.map((logo) => (
                 <a
                   key={logo.id}
                   href={logo.link}
@@ -107,13 +133,94 @@ export default function About() {
                   className="group relative"
                   style={{ width: "60px", height: "60px" }}
                 >
-                  {/* Zwart-wit SVG */}
                   <img
                     src={logo.bw}
                     alt={`${logo.name} logo`}
                     className="absolute inset-0 w-full h-full object-contain transition-opacity duration-300 group-hover:opacity-0 cursor-pointer"
                   />
-                  {/* Gekleurde SVG */}
+                  <img
+                    src={logo.color}
+                    alt={`${logo.name} logo`}
+                    className="absolute inset-0 w-full h-full object-contain opacity-0 transition-opacity duration-300 group-hover:opacity-100 cursor-pointer"
+                  />
+                </a>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <h4 className="text-2xl font-semibold mb-4">Design Skills</h4>
+            <div className="flex gap-4 flex-wrap">
+              {designSkills.map((logo) => (
+                <a
+                  key={logo.id}
+                  href={logo.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="group relative"
+                  style={{ width: "60px", height: "60px" }}
+                >
+                  <img
+                    src={logo.bw}
+                    alt={`${logo.name} logo`}
+                    className="absolute inset-0 w-full h-full object-contain transition-opacity duration-300 group-hover:opacity-0 cursor-pointer"
+                  />
+                  <img
+                    src={logo.color}
+                    alt={`${logo.name} logo`}
+                    className="absolute inset-0 w-full h-full object-contain opacity-0 transition-opacity duration-300 group-hover:opacity-100 cursor-pointer"
+                  />
+                </a>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <h4 className="text-2xl font-semibold mb-4">CMS Skills</h4>
+            <div className="flex gap-4 flex-wrap">
+              {cmsSkills.map((logo) => (
+                <a
+                  key={logo.id}
+                  href={logo.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="group relative"
+                  style={{ width: "60px", height: "60px" }}
+                >
+                  <img
+                    src={logo.bw}
+                    alt={`${logo.name} logo`}
+                    className="absolute inset-0 w-full h-full object-contain transition-opacity duration-300 group-hover:opacity-0 cursor-pointer"
+                  />
+                  <img
+                    src={logo.color}
+                    alt={`${logo.name} logo`}
+                    className="absolute inset-0 w-full h-full object-contain opacity-0 transition-opacity duration-300 group-hover:opacity-100 cursor-pointer"
+                  />
+                </a>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <h4 className="text-2xl font-semibold mb-4">
+              Version Control Skills
+            </h4>
+            <div className="flex gap-4 flex-wrap">
+              {versionControlSkills.map((logo) => (
+                <a
+                  key={logo.id}
+                  href={logo.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="group relative"
+                  style={{ width: "60px", height: "60px" }}
+                >
+                  <img
+                    src={logo.bw}
+                    alt={`${logo.name} logo`}
+                    className="absolute inset-0 w-full h-full object-contain transition-opacity duration-300 group-hover:opacity-0 cursor-pointer"
+                  />
                   <img
                     src={logo.color}
                     alt={`${logo.name} logo`}
